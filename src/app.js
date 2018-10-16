7edacafba74f3f050c2c8d34c812ae1c65108e1e
ChienNguyen <chien90udn@gmail.com>
2018-10-16 15:08:07 +0700

    Add project reactjs

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import './app.css';
+// import img from '../public/assets/download.png'
+class App extends React.Component{
+    render(){
+        return(
+            <div className="app">
+             <h1>Hello React</h1>
+               
+            </div>
+        )
+    }
+}
+export default App;
